09f133f3874cce7724b88530aaf987c5de1cb321
SheyDenis <denis.sheyer@gmail.com>
2024-05-01 23:24:45 +0300

    WLA-22: Add skeleton Django files.
    Remove some CMake related files.

--- cmake/generate-server-settings-schema.cmake
+++
@@ -1,2 +0,0 @@
-FILE(READ ${CMAKE_CURRENT_LIST_DIR}/../config/settings-schema.json SERVER_SETTINGS_SCHEMA_INPUT)
-CONFIGURE_FILE(${CMAKE_CURRENT_LIST_DIR}/../include/watch-list-server/server-settings-schema.hpp.in ${CMAKE_CURRENT_LIST_DIR}/../include/watch-list-server/server-settings-schema.hpp)
